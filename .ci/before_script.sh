0e54c33a6bab1228b13efab26c67e6ac7a1852d4
Alexandre Oliveira <rockytvbr@gmail.com>
2017-01-13 16:17:11 -0200

    Travis CI: setup SED

--- .ci/before_script.sh
+++ .ci/before_script.sh
@@ -5,3 +5,10 @@ echo "Building using dummy assemblies from KSP-KOS/KSP_LIB"
 wget --quiet https://github.com/KSP-KOS/KSP_LIB/blob/master/kos-${KSP_VERSION}.tar?raw=true -O lib-${KSP_VERSION}.tar
 
 tar -xvf lib-${KSP_VERSION}.tar
+
+buildtag=${TRAVIS_TAG}
+if [[ ! -v $TRAVIS_TAG ]]; then 
+    buildtag=${TRAVIS_COMMIT}
+fi
+
+sed -i "s/\(.*PROGRAM_VERSION = \"\)Custom\(\".*\)/\1${buildtag}\2/g" Common/Common.cs
